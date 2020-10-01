b95af5e4bff25fae4c681022b977e288e707cfd1
Snehil <snehilverma488@gmail.com>
2020-10-01 07:19:08 +0530

    [Snehil] Add : Generating Any Of Three Random Options

--- SnakeAndLadderSimulator/Program.cs
+++ SnakeAndLadderSimulator/Program.cs
@@ -6,6 +6,9 @@ namespace SnakeAndLadderSimulator
     {
         public const int STARTING_POSITION = 0;
         public const int WINNING_POSITION = 100;
+        public const int NO_PLAY = 1;
+        public const int LADDER = 2;
+        public const int SNAKE = 3;
         //Returns Output Of Die Roll
         public static int rollTheDie()
         {
@@ -13,10 +16,32 @@ namespace SnakeAndLadderSimulator
             int numOnDie = random.Next(1, 7);
             return numOnDie;
         }
+        //Returns Increment Or Decrement to be Made with Current Position
+        public static int newPosition(int numOnDie)
+        {
+            int toAdd = 0;
+            Random random = new Random();
+            int toDo = random.Next(1, 4);
+            switch (toDo)
+            {
+                case NO_PLAY:
+                    break;
+                case LADDER:
+                    toAdd = numOnDie;
+                    break;
+                case SNAKE:
+                    toAdd = -numOnDie;
+                    break;
+            }
+            return toAdd;
+        }
         static void Main(string[] args)
         {
             int currentPosition = STARTING_POSITION;
+            //Rolling Die
             int numOnDie = rollTheDie();
+            //Opting for an option out of three
+            int toAdd = newPosition(numOnDie);
         }
     }
 }
