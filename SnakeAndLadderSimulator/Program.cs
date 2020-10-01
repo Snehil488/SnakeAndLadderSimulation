f24197577f61cc3354ce713f1622894790f8fab9
Snehil <snehilverma488@gmail.com>
2020-10-01 07:14:04 +0530

    [Snehil] Add : Dice Rolling Method Added

--- SnakeAndLadderSimulator/Program.cs
+++ SnakeAndLadderSimulator/Program.cs
@@ -6,9 +6,17 @@ namespace SnakeAndLadderSimulator
     {
         public const int STARTING_POSITION = 0;
         public const int WINNING_POSITION = 100;
+        //Returns Output Of Die Roll
+        public static int rollTheDie()
+        {
+            Random random = new Random();
+            int numOnDie = random.Next(1, 7);
+            return numOnDie;
+        }
         static void Main(string[] args)
         {
             int currentPosition = STARTING_POSITION;
+            int numOnDie = rollTheDie();
         }
     }
 }
