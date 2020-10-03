47f67c4840ce1fa9de02eb2d7f23281225d40a71
Snehil488 <71805322+Snehil488@users.noreply.github.com>
2020-10-03 13:08:13 +0530

    Correcting Method Name Using Pascal Convention

--- SnakeAndLadderSimulator/Program.cs
+++ SnakeAndLadderSimulator/Program.cs
@@ -7,7 +7,7 @@ namespace SnakeAndLadderSimulator
         public const int STARTING_POSITION = 0;
         public const int WINNING_POSITION = 100;
         //Returns Output Of Die Roll
-        public static int rollTheDie()
+        public static int RollTheDie()
         {
             Random random = new Random();
             int numOnDie = random.Next(1, 7);
@@ -16,7 +16,7 @@ namespace SnakeAndLadderSimulator
         static void Main(string[] args)
         {
             int currentPosition = STARTING_POSITION;
-            int numOnDie = rollTheDie();
+            int numOnDie = RollTheDie();
         }
     }
 }
